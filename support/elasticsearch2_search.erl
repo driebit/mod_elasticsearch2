01d06add277e6b54457d71e9e5edd8eb8533c5e3
Marc Worrell <marc@worrell.nl>
2022-07-21 15:53:30 +0200

    Fix a problem with stored queries that have multiple terms.

--- support/elasticsearch2_search.erl
+++ support/elasticsearch2_search.erl
@@ -145,7 +145,7 @@ build_function_score(Query, Context) ->
     FunctionScore = #{
         <<"query">> => #{
                 <<"bool">> => #{
-                    <<"must">> => lists:filtermap(fun(Q) -> map_query(Q, Context) end, Query),
+                    <<"must">> => lists:flatten( lists:filtermap(fun(Q) -> map_query(Q, Context) end, Query) ),
                     <<"filter">> => build_filter(Query, Context)
                 }
         },
@@ -180,8 +180,8 @@ build_query(Query, {From, Size}, Context) ->
 build_filter(Query, Context) ->
     #{
         <<"bool">> => #{
-            <<"must">> => lists:filtermap(fun(Q) -> map_must(Q, Context) end, Query),
-            <<"must_not">> => lists:filtermap(fun(Q) -> map_must_not(Q, Context) end, Query)
+            <<"must">> => lists:flatten( lists:filtermap(fun(Q) -> map_must(Q, Context) end, Query) ),
+            <<"must_not">> => lists:flatten( lists:filtermap(fun(Q) -> map_must_not(Q, Context) end, Query) )
         }
     }.
 
